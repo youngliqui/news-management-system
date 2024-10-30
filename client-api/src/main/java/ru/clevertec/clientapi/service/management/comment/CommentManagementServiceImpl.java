215e68357049f69232f0dae07899bc65c3f33327
youngliqui <super.pav-79@yandex.by>
2024-10-30 23:35:51 +0300

    feat: Implemented creation, update and deletion services for news and comments

--- client-api/src/main/java/ru/clevertec/clientapi/service/management/comment/CommentManagementServiceImpl.java
+++ client-api/src/main/java/ru/clevertec/clientapi/service/management/comment/CommentManagementServiceImpl.java
@@ -5,32 +5,54 @@ import org.springframework.stereotype.Service;
 import ru.clevertec.clientapi.dto.CommentCreateDTO;
 import ru.clevertec.clientapi.dto.CommentInfoDTO;
 import ru.clevertec.clientapi.dto.CommentUpdateDTO;
+import ru.clevertec.clientapi.entity.CommentEntity;
 import ru.clevertec.clientapi.entity.NewsEntity;
+import ru.clevertec.clientapi.exception.CommentNotFoundException;
+import ru.clevertec.clientapi.mapper.CommentMapper;
 import ru.clevertec.clientapi.repository.CommentRepository;
 
 @Service
 @RequiredArgsConstructor
 public class CommentManagementServiceImpl implements CommentManagementService {
-    private CommentRepository commentRepository;
+    private final CommentRepository commentRepository;
+    private final CommentMapper commentMapper;
 
 
     @Override
     public CommentInfoDTO createComment(NewsEntity news, CommentCreateDTO commentCreateDTO) {
-        return null;
+        CommentEntity newComment = commentMapper.commentCreateDTOToComment(commentCreateDTO, news);
+
+        return commentMapper.commentToCommentInfoDTO(newComment);
     }
 
     @Override
     public CommentInfoDTO updateComment(Long commentId, CommentUpdateDTO commentUpdateDTO) {
-        return null;
+        CommentEntity comment = commentRepository.findById(commentId)
+                .orElseThrow(() ->
+                        new CommentNotFoundException("Comment with id=" + commentId + " was not found"));
+
+        CommentEntity updatedComment = commentMapper.updateComment(comment, commentUpdateDTO);
+
+        return commentMapper.commentToCommentInfoDTO(updatedComment);
     }
 
     @Override
     public CommentInfoDTO patchComment(Long commentId, CommentUpdateDTO commentUpdateDTO) {
-        return null;
+        CommentEntity comment = commentRepository.findById(commentId)
+                .orElseThrow(() ->
+                        new CommentNotFoundException("Comment with id=" + commentId + " was not found"));
+
+        CommentEntity patchedComment = commentMapper.patchComment(comment, commentUpdateDTO);
+
+        return commentMapper.commentToCommentInfoDTO(patchedComment);
     }
 
     @Override
     public void deleteCommentById(Long commentId) {
+        CommentEntity comment = commentRepository.findById(commentId)
+                .orElseThrow(() ->
+                        new CommentNotFoundException("Comment with id=" + commentId + " was not found"));
 
+        commentRepository.delete(comment);
     }
 }
--- client-api/src/main/java/ru/clevertec/clientapi/service/management/news/NewsManagementServiceImpl.java
+++ client-api/src/main/java/ru/clevertec/clientapi/service/management/news/NewsManagementServiceImpl.java
@@ -5,30 +5,52 @@ import org.springframework.stereotype.Service;
 import ru.clevertec.clientapi.dto.NewsCreateDTO;
 import ru.clevertec.clientapi.dto.NewsInfoDTO;
 import ru.clevertec.clientapi.dto.NewsUpdateDTO;
+import ru.clevertec.clientapi.entity.NewsEntity;
+import ru.clevertec.clientapi.exception.NewsNotFoundException;
+import ru.clevertec.clientapi.mapper.NewsMapper;
 import ru.clevertec.clientapi.repository.NewsRepository;
 
 @Service
 @RequiredArgsConstructor
 public class NewsManagementServiceImpl implements NewsManagementService {
     private final NewsRepository newsRepository;
+    private final NewsMapper newsMapper;
 
     @Override
     public NewsInfoDTO createNews(NewsCreateDTO newsCreateDTO) {
-        return null;
+        NewsEntity newNews = newsMapper.newsCreateDTOToNews(newsCreateDTO);
+
+        return newsMapper.newsToNewsInfoDTO(newsRepository.save(newNews));
     }
 
     @Override
     public NewsInfoDTO updateNews(Long newsId, NewsUpdateDTO newsUpdateDTO) {
-        return null;
+        NewsEntity newsEntity = newsRepository.findById(newsId)
+                .orElseThrow(() ->
+                        new NewsNotFoundException("News with id=" + newsId + " was not found"));
+
+        NewsEntity updatedNews = newsMapper.updateNews(newsEntity, newsUpdateDTO);
+
+        return newsMapper.newsToNewsInfoDTO(updatedNews);
     }
 
     @Override
     public NewsInfoDTO patchNews(Long newsId, NewsUpdateDTO newsUpdateDTO) {
-        return null;
+        NewsEntity newsEntity = newsRepository.findById(newsId)
+                .orElseThrow(() ->
+                        new NewsNotFoundException("News with id=" + newsId + " was not found"));
+
+        NewsEntity patchedNews = newsMapper.patchNews(newsEntity, newsUpdateDTO);
+
+        return newsMapper.newsToNewsInfoDTO(patchedNews);
     }
 
     @Override
     public void deleteNewsById(Long newsId) {
+        NewsEntity newsEntity = newsRepository.findById(newsId)
+                .orElseThrow(() ->
+                        new NewsNotFoundException("News with id=" + newsId + " was not found"));
 
+        newsRepository.delete(newsEntity);
     }
 }
